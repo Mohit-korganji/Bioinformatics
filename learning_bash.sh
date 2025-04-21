3b6893b0a0bff7745c621386f28d20faf34cf0a8
Mohit-korganji <mohit.k.korganji@wmich.edu>
2025-04-21 08:25:53 -0400

    Create learning_bash.sh

---
+++ learning_bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+greeting="Hello"
+your_name="Mohit"
+
+echo "$greeting $your_name!"
+echo "This script is helping you learn bash."
+
+mkdir -p /Users/mohit/Desktop/bioinformatics/bash_scripts
+mv /Users/mohit/Desktop/bash_template.txt /Users/mohit/Desktop/bioinformatics/bash_scripts/
+mv /Users/mohit/Desktop/bioinformatics/bash_scripts/bash_template.txt /Users/mohit/Desktop/bioinformatics/bash_scripts/bash_template.sh
